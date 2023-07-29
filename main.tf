c46f0949aa9ff5415d49f81ed39e1d7ba9232f29
tom-puntillo <tom.puntillo@gmail.com>
2023-07-29 22:58:34 +0000

    added role and policy resource blocks to main.tf

--- main.tf
+++ main.tf
@@ -56,3 +56,47 @@ resource "tls_private_key" "jenkins_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+resource "aws_iam_policy" "jenkins_policy" {
+  name        = "jenkins_policy"
+  path        = "/"
+  description = "My jenkins policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Action" : [
+          "s3:ListAllMyBuckets"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "arn:aws:s3:::*"
+      },
+      {
+        "Action" : "s3:*",
+        "Effect" : "Allow",
+        "Resource" : ["arn:aws:s3:::thomas-week-20-luit-blue-bucket-2023", "arn:aws:s3:::thomas-week-20-luit-blue-bucket-2023/*"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "jenkins_role" {
+  name = "jenkins_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins-role-policy-attach" {
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = aws_iam_policy.jenkins_policy.arn
+}
