9dd7f5fee126a3a89b67c7c8c3077ddc0fbb355e
tom-puntillo <tom.puntillo@gmail.com>
2023-07-30 02:17:41 +0000

    Added EC2 Instance to main.tf

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "thomas-week-20-luit-blue-bucket-2023"
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "thomas-week-20-luit-blue-bucket-2023-2"
 
   tags = {
     Name = "Week 20 bucket"
@@ -57,6 +57,12 @@ resource "tls_private_key" "jenkins_key" {
   rsa_bits  = 4096
 }
 
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "jenkins_ssh"
+  public_key = tls_private_key.jenkins_key.public_key_openssh
+}
+
+
 resource "aws_iam_policy" "jenkins_policy" {
   name        = "jenkins_policy"
   path        = "/"
@@ -74,7 +80,7 @@ resource "aws_iam_policy" "jenkins_policy" {
       {
         "Action" : "s3:*",
         "Effect" : "Allow",
-        "Resource" : ["arn:aws:s3:::thomas-week-20-luit-blue-bucket-2023", "arn:aws:s3:::thomas-week-20-luit-blue-bucket-2023/*"]
+        "Resource" : ["arn:aws:s3:::thomas-week-20-luit-blue-bucket-2023-2", "arn:aws:s3:::thomas-week-20-luit-blue-bucket-2023-2/*"]
       }
     ]
   })
@@ -96,7 +102,40 @@ resource "aws_iam_role" "jenkins_role" {
   })
 }
 
+resource "aws_iam_instance_profile" "jenkins_profile" {
+  name = "jenkins_profile"
+  role = "jenkins_role"
+}
+
 resource "aws_iam_role_policy_attachment" "jenkins-role-policy-attach" {
   role       = aws_iam_role.jenkins_role.name
   policy_arn = aws_iam_policy.jenkins_policy.arn
+}
+
+resource "aws_instance" "jenkins-server" {
+  ami                  = "ami-0f9ce67dcf718d332"
+  instance_type        = "t2.micro"
+  security_groups      = ["jenkins-security-group"]
+  key_name             = "jenkins_ssh"
+  iam_instance_profile = "jenkins_profile" # Use the IAM instance profile name here
+  user_data            = <<EOF
+ #!/bin/bash
+
+yum update –y
+wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum upgrade -y
+amazon-linux-extras install java-openjdk11 -y
+yum install jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
+sudo chmod 755 /var/lib/jenkins/secrets
+sudo chmod 644 /var/lib/jenkins/secrets/initialAdminPassword
+
+EOF
+
+  tags = {
+    Name = "jenkins-server"
+  }
 }
